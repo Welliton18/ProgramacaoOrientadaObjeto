99496495e4d1f7dff2fa6510510ab427464e458f
Welliton Luiz Becker <chaank18@gmail.com>
2022-02-16 20:00:57 -0300

    Alterado

---
+++ Aula/Aula/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula {
+    class Atividade {
+
+        static void Main(){
+            string sNome;
+            string sDataNascimento;
+            string sRg;
+            char sSexo;
+            double iSalario;
+
+            Console.WriteLine("Qual o seu Nome?");
+            sNome = Console.ReadLine();
+
+            Console.WriteLine("Qual sua Data de nascimento?");
+            sDataNascimento = Console.ReadLine();
+
+            Console.WriteLine("Qual o seu RG?");
+            sRg = Console.ReadLine();
+
+            Console.WriteLine("Qual o seu Sexo(M ou F)?");
+            sSexo = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qual o seu Salário?");
+            iSalario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("O Senhor(a) "+ sNome+ ", portador(a) do RG de número "+ sRg+
+                   ", nascido em "+ sDataNascimento + ", do sexo ("+sSexo+"), está registrado com o salário de R$"+ iSalario+".")
+        }
+    }
+}
