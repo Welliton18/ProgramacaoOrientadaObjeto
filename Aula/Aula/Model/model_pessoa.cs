e3b5ca6b9cc222e970a78c6e6e8a180b69aef971
Welliton Luiz Becker <chaank18@gmail.com>
2022-02-16 20:44:29 -0300

    Alterado

---
+++ Aula/Aula/Model/model_pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model {
+    class ModelPessoa {
+
+        private string nome;
+        private string dataNascimento;
+        private string rg;
+        private char sexo;
+        private double salario;
+
+        public string Nome { get => nome; set => nome = value; }
+        public string DataNascimento { get => dataNascimento; set => dataNascimento = value; }
+        public string Rg { get => rg; set => rg = value; }
+        public char Sexo { get => sexo; set => sexo = value; }
+        public double Salario { get => salario; set => salario = value; }
+    }
+}
---
+++ Aula/Aula/atividade_declaracao_variaveis.cs
@@ -0,0 +1,29 @@
+﻿using Model;
+using System;
+
+class AtividadeDeclaracaoVariaveis {
+
+    static void Main(){
+        ModelPessoa oPessoa = new ModelPessoa();
+
+        Console.WriteLine("Qual o seu Nome?");
+        oPessoa.Nome = Console.ReadLine();
+
+        Console.WriteLine("Qual sua Data de nascimento?");
+        oPessoa.DataNascimento = Console.ReadLine();
+
+        Console.WriteLine("Qual o seu RG?");
+        oPessoa.Rg = Console.ReadLine();
+
+        Console.WriteLine("Qual o seu Sexo(M ou F)?");
+        oPessoa.Sexo = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Qual o seu Salário?");
+        oPessoa.Salario = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("O Senhor(a) " + oPessoa.Nome + ", portador(a) do RG de número " + oPessoa.Rg +
+                            ", nascido em " + oPessoa.DataNascimento + ", do sexo (" + oPessoa.Sexo + 
+                            "), está registrado com o salário de R$" + oPessoa.Salario + ".");
+    }
+
+}
